07522e9aca0a9fc6186bbdd1eb33ee081b07febd
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-01-02 18:22:12 +0000

    Add project files.

---
+++ Algorithms/main.py
@@ -0,0 +1,9 @@
+"""
+task:add 2 numbers
+1. start
+2. declare a variabale num1,num2 and sum
+3. value of num 1 and num 2 and read the value
+4. Add number one and number two 
+5.display the sum |
+6. stop
+"""
